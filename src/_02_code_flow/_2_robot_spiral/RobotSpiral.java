0f2918ccfba551825e74c2128fdd22ba508c1e2b
Marcus Solt <marcussolt723@gmail.com>
2022-09-01 19:58:29 -0700

    flame, crazy, rob, square done.

--- src/_02_code_flow/_2_robot_spiral/RobotSpiral.java
+++ src/_02_code_flow/_2_robot_spiral/RobotSpiral.java
@@ -12,33 +12,39 @@ public class RobotSpiral {
 	public static void main(String[] args) {
 		
 		// Create a new Robot
-		
+		Robot rob = new Robot();
 		// Set your robot's pen down 
-		
+		rob.penDown();
 		// SPEED. Set the robot to go at max speed (100)
-
+		rob.setSpeed(100);
 		// COUNT. Create an int variable that will count how many lines of the spiral we have drawn.
 			//        Start its value as zero.
-
+		int lines = 0;
 		// LOOP. Start a while loop to repeat the COLOR, DRAW, TURN, and COUNT code below until 50 lines have been drawn
-
-			// COLOR.   Have the robot set a random pen color:      setRandomPenColor()
-
-			// DRAW.    Move the robot (5*count) pixels
-			//          count is the name of the variable you created earlier
-
-			// TURN.    Turn the robot (360/7) degrees to the right
-
-			// COUNT.   Increase the count of how many lines have been drawn so far ( count+=1 )
-
-			// Change the robot pen width to the current value of the count variable 
-
-
-		//  End the while loop here
-
-		
-		// Run the program. 
-		// Check the pattern against the picture in the recipe. If it matches, you are done!
-		
-	}
+while (lines < 50) {
+	rob.setRandomPenColor();
+		rob.move(5*lines);
+		rob.turn(360/7);
+		lines += 1;
+	rob.setPenWidth(lines);
 }
+}
+		// COLOR.   Have the robot set a random pen color:      setRandomPenColor()
+
+					// DRAW.    Move the robot (5*count) pixels
+					//          count is the name of the variable you created earlier
+
+					// TURN.    Turn the robot (360/7) degrees to the right
+
+					// COUNT.   Increase the count of how many lines have been drawn so far ( count+=1 )
+
+					// Change the robot pen width to the current value of the count variable 
+
+
+				//  End the while loop here
+
+				
+				// Run the program. 
+				// Check the pattern against the picture in the recipe. If it matches, you are done!
+}
+	
