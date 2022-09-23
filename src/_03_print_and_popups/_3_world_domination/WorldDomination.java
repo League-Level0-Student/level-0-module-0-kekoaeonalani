cfcaeb3dddd7ca2bb1dcbd2bcc3c50237de57e62
league <league@wts14-3.attlocal.net>
2022-09-22 20:01:29 -0700

    greet, madlib, quiz, worlddom, for quiz show the score

--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -8,11 +8,17 @@ public class WorldDomination {
 	public static void main(String[] args) {
 		
 		// 1. Ask the user if they know how to write code.
-
+String code = JOptionPane.showInputDialog("Do you know how to code?");
 		// 2. If they say "yes", tell them they will rule the world.
-
+if (code.equals ("yes")) {
+	String prompt =  "You rule the world!";
+	JOptionPane.showMessageDialog(null, prompt);
+}
 		// 3. Otherwise, tell them to sign up for classes at The League.
-
+else {
+	String hmm =  "Hmm. You better sign up for classes at The Leauge.";
+	JOptionPane.showMessageDialog(null, hmm);
+}
 	}
 }
 
--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -1,22 +1,49 @@
 package _03_print_and_popups._4_quiz_game;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	public static void main(String[] args) {
 		
 		// Create a variable to hold the user's score. Set it equal to zero. 
-		
+		int score = 0;
 		// ASK A QUESTION AND CHECK THE ANSWER
 		
 				// 2.  Ask the user a question 
-				
+		String water = JOptionPane.showInputDialog("Is being dehydrated good?");
 				// 3.  Use an if statement to check if their answer is correct
-				
-				// 4.  if the user's answer was correct, add one to their score 
+				if (water.equals("no")) {
+					 JOptionPane.showMessageDialog(null, "Good. I hope you are hydrated!");
+					// 4.  if the user's answer was correct, add one to their score 
+					score += 1;
+				}
+				else {
+				JOptionPane.showMessageDialog(null, "Wrong. Drink a cup of water right now.");
+				score -= 1;
+				}
 		
 		// MAKE MORE QUESTIONS. Ask more questions by repeating the above 
 				// Option: Subtract a point from their score for a wrong answer
-		
-		
+		String type = JOptionPane.showInputDialog("Should you learn how to type?");
+		if (type.equals("yes")) {
+			JOptionPane.showMessageDialog(null, "You are two steps ahead of most kids.");
+			score += 1;
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "Do you even have a computer?!");
+			score -= 1;
+		}
+			
+		String life = JOptionPane.showInputDialog("What country has the highest life expectancy?");
+				if (life.equals("Hong Kong")) {
+					JOptionPane.showMessageDialog(null,"Wow. Correct!");
+					score += 1;
+				}
+				else {
+					JOptionPane.showMessageDialog(null, "Wrong.");
+					score -= 1;
+				}
+
 		// After all the questions have been asked, tell the user their final score 
 		
 	}
